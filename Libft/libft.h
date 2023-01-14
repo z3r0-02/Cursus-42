8dad54c8e5a8b59998d266871faf1e9182aefac0
z3r0-02 <nika.b2@hotmail.com>
2023-01-14 15:55:48 +0100

    14.1. add part 2

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: thbui <thbui@student.42prague.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 14:56:27 by thbui             #+#    #+#             */
-/*   Updated: 2023/01/14 00:55:18 by thbui            ###   ########.fr       */
+/*   Updated: 2023/01/14 03:12:52 by thbui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,17 @@ int				ft_tolower(int c);
 int				ft_toupper(int c);
 char			*ft_strdup(char *src);
 void			*ft_calloc(size_t nmemb, size_t size);
+
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			**ft_split(char const *s, char c);
+char			*ft_itoa(int n);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void			ft_striteri(char *s, void (*f)(unsigned int, char*));
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
 
 #endif
