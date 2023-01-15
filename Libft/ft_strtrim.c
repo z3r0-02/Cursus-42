ef804a89b3c1a1f795911d7062646a3119b3d34e
z3r0-02 <nika.b2@hotmail.com>
2023-01-15 20:23:40 +0100

    nearly jumped of the bridge...

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,39 +6,11 @@
 /*   By: thbui <thbui@student.42prague.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 15:48:08 by thbui             #+#    #+#             */
-/*   Updated: 2023/01/15 20:01:12 by thbui            ###   ########.fr       */
+/*   Updated: 2023/01/15 20:22:06 by thbui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// int check_variable(const char *s1, const char *set)
-// {
-// 	char	c;
-// 	int	i;
-
-// 	c = s1[0];
-// 	i = 0;
-// 	while (s1[i])
-// 	{
-// 		if (c != s1[i])
-// 			return (1);
-// 		else if (c != set[0])
-// 			return (1);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
 
 int	set_check(const char c, char const *set)
 {
@@ -67,7 +39,7 @@ int lenght_check(const char *s1, const char *set)
 		yes++;
 		i++;
 	}
-	while (s1[i] != s1[len] && set_check(s1[len], set))
+	while (i < len && set_check(s1[len], set))
 	{
 		len--;
 		yes++;
@@ -96,8 +68,6 @@ char *ft_strtrim(char const *s1, char const *set)
 	res = malloc((len + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
-	// if (check_variable(s1, set) == 0)
-	// 	return (NULL);
 	while (set[0] && set_check(s1[s], set))
 		s++;
 	while (i < len)
@@ -108,11 +78,3 @@ char *ft_strtrim(char const *s1, char const *set)
 	res[i] = '\0';
 	return (res);
 }	
-
-// #include <stdio.h>
-// int	main()
-// {
-// 	const char s1[] = "       ";
-// 	printf("%s", ft_strtrim(s1, " "));
-// 	return (0);
-// }
