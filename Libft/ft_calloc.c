d0a5d26d9bfd35523130e65a161b83d63874992f
z3r0-02 <nika.b2@hotmail.com>
2023-02-05 00:27:13 +0100

    calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: thbui <thbui@student.42prague.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:36:02 by thbui             #+#    #+#             */
-/*   Updated: 2023/01/19 13:42:56 by thbui            ###   ########.fr       */
+/*   Updated: 2023/02/05 00:26:36 by thbui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,30 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (!res)
 		return (NULL);
 	ft_bzero (res, size * nmemb);
+	return (res);
+}
+
+//-----------------------------------------------------------------------------//
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*res;
+
+	res = malloc(size * nmemb);
+	if (!res)
+		return (NULL);
+	ft_bzero(res, size * nmemb);
+	return (res);
+}
+
+//----------------------------------------------------------------------------//
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*res;
+
+	res = malloc(size * nmemb);
+	if (res)
+		ft_bzero(res, size * nmemb);
 	return (res);
 }
