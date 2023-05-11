6772155a759851515a2cb088433d2cbafc8c48b3
z3r0-02 <nika.b2@hotmail.com>
2023-05-11 15:07:52 +0200

    norm

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,13 +6,13 @@
 /*   By: thbui <thbui@student.42prague.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 22:59:25 by thbui             #+#    #+#             */
-/*   Updated: 2023/05/11 14:58:58 by thbui            ###   ########.fr       */
+/*   Updated: 2023/05/11 15:07:23 by thbui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-void ft_send_each_bits(int PID, char c)
+void	ft_send_each_bits(int pid, char c)
 {
 	static int	bit;
 
@@ -20,9 +20,9 @@ void ft_send_each_bits(int PID, char c)
 	while (bit < 8)
 	{
 		if ((c & 1 << bit) != 0)	//shifting bit value of decimal number to the left
-			kill(PID, SIGUSR1);		//sends 1
+			kill(pid, SIGUSR1);		//sends 1
 		else if ((c & 1 << bit) == 0)
-			kill(PID, SIGUSR2);		//sends 0
+			kill(pid, SIGUSR2);		//sends 0
 		usleep(128);
 		bit++;
 	}
@@ -30,17 +30,17 @@ void ft_send_each_bits(int PID, char c)
 
 int	main(int argc, char **argv)
 {
-	int	PID;
+	int	pid;
 	static int	i;
 
 	i = 0;
-	PID = ft_atoi(argv[1]);
+	pid = ft_atoi(argv[1]);
 
 	if (argc != 3 || !ft_strchr("1234567890", argv[1][i]))
 		return (ft_printf("error"));
 	while (argv[2][i] != '\0')
 	{
-		ft_send_each_bits(PID, argv[2][i]);		//converting each int to bit and sending it to PID
+		ft_send_each_bits(pid, argv[2][i]);		//converting each int to bit and sending it to PID
 		i++;
 	}
 }
