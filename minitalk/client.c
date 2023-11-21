e68a56378e87f34303aa1c31a0e246730ab07ea2
z3r0-02 <nika.b2@hotmail.com>
2023-11-21 22:12:17 +0100

    minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: thbui <thbui@student.42prague.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 22:59:25 by thbui             #+#    #+#             */
-/*   Updated: 2023/05/11 15:07:23 by thbui            ###   ########.fr       */
+/*   Updated: 2023/11/21 22:10:35 by thbui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,27 @@ void	ft_send_each_bits(int pid, char c)
 	bit = 0;
 	while (bit < 8)
 	{
-		if ((c & 1 << bit) != 0)	//shifting bit value of decimal number to the left
+		if ((c & 1 << bit) != 0)	//shifting bit value of decimal number to the left and testing if bit is set to 1
 			kill(pid, SIGUSR1);		//sends 1
-		else if ((c & 1 << bit) == 0)
+		else if ((c & 1 << bit) == 0)	//bit is set to 0
 			kill(pid, SIGUSR2);		//sends 0
-		usleep(128);
+		usleep(128);				//more time might be needed
 		bit++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
+	int			pid;
 	static int	i;
 
 	i = 0;
 	pid = ft_atoi(argv[1]);
-
 	if (argc != 3 || !ft_strchr("1234567890", argv[1][i]))
 		return (ft_printf("error"));
 	while (argv[2][i] != '\0')
 	{
-		ft_send_each_bits(pid, argv[2][i]);		//converting each int to bit and sending it to PID
+		ft_send_each_bits(pid, argv[2][i]);		//converting each int to bit and sending it to PID with the function
 		i++;
 	}
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: thbui <thbui@student.42prague.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 22:59:27 by thbui             #+#    #+#             */
-/*   Updated: 2023/05/11 15:01:27 by thbui            ###   ########.fr       */
+/*   Updated: 2023/11/21 18:52:43 by thbui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	ft_sig_handler(int sig)
 	static int	bit;
 
 	if (sig == SIGUSR1)
-		i |= (0x01 << bit);
+		i |= (0x01 << bit);		//0x01 is the same as 1, |= is a bitwise OR, it will compare i with bit-th bit if both are not 0 it will change to 1 without affecting the rest
 	bit++;
-	if (bit == 8)
+	if (bit == 8)				//when it's the last one it will write out the value of i, and resets
 	{
 		write(1, &i, 1);
 		bit = 0;
@@ -28,14 +28,14 @@ void	ft_sig_handler(int sig)
 	}
 }
 
-int main()		//only signals to be used: SIGUSR1, SIGUSR2
+int	main(void)		//only signals to be used: SIGUSR1, SIGUSR2
 {
 	int	pid;						//integer value
 
 	pid = getpid();					//getpid() returns the PID of the process and stores it in PID
 	ft_printf("pid: %d\n", pid);
-    signal(SIGUSR1, ft_sig_handler);
-    signal(SIGUSR2, ft_sig_handler);
-    while (1)
+	signal(SIGUSR1, ft_sig_handler);	//defines the way it should respond to a specific signal
+	signal(SIGUSR2, ft_sig_handler);	//application-level use signals
+	while (1)
 		pause();					//pause until a signal is received
 }
